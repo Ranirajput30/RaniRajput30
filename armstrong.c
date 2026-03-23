4d02107278bc3625771c9148f665270ba2c70b99
Ranirajput30 <25111cn007@glbitm.ac.in>
2026-03-23 21:47:52 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,n,r,sum;
+
+    for(i=1;i<=100;i++)
+    {
+        n=i;
+        sum=0;
+
+        while(n>0)
+        {
+            r=n%10;
+            sum+=r*r*r;
+            n/=10;
+        }
+
+        if(sum==i)
+            printf("%d ",i);
+    }
+
+    return 0;
+}
