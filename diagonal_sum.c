a981c4bc5f74a481b02c9cc88cbbb93756eae45d
Ranirajput30 <25111cn007@glbitm.ac.in>
2026-03-24 10:28:37 +0530

    Create diagonal_sum.c

---
+++ diagonal_sum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], i, j, n, sum=0;
+
+    printf("Enter order of matrix: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<n;i++)
+        sum += a[i][i];
+
+    printf("Sum of diagonal elements = %d",sum);
+
+    return 0;
+}
